48149156273b6888cb38f0ce8f18276ff7e14a5e
Pedro Lima <pedro.nym@gmail.com>
2016-08-07 00:30:02 +0100

    Cleaned up a bit of the code and added some new functionality.

--- js/circle.js
+++ js/circle.js
@@ -1,38 +1,38 @@
-var Circle = function(e, options)
+var Circle = function (e, options)
 {
-	if(options) {
+	if (options) {
 		this.options = options;
 	} else {
 		this.options = {};
 	}
 
-	this.circleSize = this.getRandomNumber();
-    this.circleColor = this.getRandomColor();
+	this.circleSize    = this.getRandomNumber();
+    this.circleColor   = this.getRandomColor();
     this.circleOpacity = this.getRandomOpacity();
-    this.targetX= this.getRandomX();
-   	this.targetY = this.getRandomY();
+    this.targetX       = this.getRandomX();
+   	this.targetY       = this.getRandomY();
 
-    if(e.type === "keyup"){
+    if (e.type === "keyup") {
 		this.circleChar = KeyDecoder.decode(e.keyCode);
 	}
-	
-	if(e.type === "mouseup"){
-		this.circleOrigin = {x:e.clientX, y:e.clientY};
+
+	if (e.type === "mouseup") {
+		this.circleOrigin = { x:e.clientX, y:e.clientY };
 	}
-	
-	if(this.circleChar){
+
+	if (this.circleChar) {
 		this.circle = $('<div class="circle"><div class="circleChar"><h1>'+ this.circleChar +'</h1></div></div>');
 	} else {
 		this.circle = $('<div class="circle"><div class="circleChar"></div></div>');
 	}
-	
-	if(e.type === "keyup"){
+
+	if (e.type === "keyup") {
 		TweenMax.set(this.circle, {scale:this.getRandomOpacity(), backgroundColor:this.circleColor, alpha:0, top:"50%", left:"50%"});
-	} else if(e.type === "mouseup"){
+	} else if (e.type === "mouseup") {
 		TweenMax.set(this.circle, {scale:this.getRandomOpacity(), left:this.circleOrigin.x, top:this.circleOrigin.y, backgroundColor:this.circleColor, alpha:0});
 	}
-    
-    if(options && options.fade){
+
+    if (options && options.fade) {
     	TweenMax.to(this.circle, this.getRandomTime(), {top:this.targetY, left:this.targetX, alpha:this.circleOpacity, ease:"Expo.easeOut", onComplete:this.fadeOut, onCompleteParams:this.circle});
     } else {
     	TweenMax.to(this.circle, this.getRandomTime(), {top:this.targetY, left:this.targetX, alpha:this.circleOpacity, ease:"Expo.easeOut"});
@@ -41,34 +41,34 @@ var Circle = function(e, options)
     return this.circle;
 };
 
-Circle.prototype.getRandomNumber = function() { 
-	return Math.floor(Math.random() * (150 - 25)) + 150; 
+Circle.prototype.getRandomNumber = function() {
+	return Math.floor(Math.random() * (150 - 25)) + 150;
 }
 
-Circle.prototype.getRandomColor = function() { 
+Circle.prototype.getRandomColor = function() {
 	return Utils.generateRandomColor();
 }
 
-Circle.prototype.getRandomOpacity = function() { 
-	return (Math.random() * (1 - 0.5) + 0.5) 
+Circle.prototype.getRandomOpacity = function() {
+	return (Math.random() * (1 - 0.5) + 0.5)
 };
 
-Circle.prototype.getRandomTime = function() { 
-	return (Math.random() * 3) + 1; 
+Circle.prototype.getRandomTime = function() {
+	return (Math.random() * 3) + 1;
 }
 
-Circle.prototype.getRandomX = function() { 
-	return Math.floor(Math.random() * ($(window).width() - 1) + 1); 
+Circle.prototype.getRandomX = function() {
+	return Math.floor(Math.random() * ($(window).width() - 1) + 1);
 }
 
-Circle.prototype.getRandomY = function() { 
-	return Math.floor(Math.random() * ($(window).height() - 1) + 1); 
+Circle.prototype.getRandomY = function() {
+	return Math.floor(Math.random() * ($(window).height() - 1) + 1);
 }
 
-Circle.prototype.fadeOut = function(circle) { 
-	TweenMax.to(circle, 2, {delay:0.5, alpha:0, 
-		onComplete:function(){
+Circle.prototype.fadeOut = function(circle) {
+	TweenMax.to(circle, 2, {delay:0.5, alpha:0,
+		onComplete:function() {
 			TweenMax.killTweensOf(circle); circle.remove();
 		}
-	}); 
+	});
 }
--- js/main.js
+++ js/main.js
@@ -1,104 +1,160 @@
 $(document).ready(function() {
-    
-    var stage = $('#stage');
-    var tools = $('#tools');
-    var appName = $("#appName");
-    
-    var circles = [];
-    var circlesPerBurst = 10;
 
-	$(window).on('keyup', keyPress);
-	stage.on('mouseup', mousePress);
-	
-	tools.on('click', 'button', function(e){
-		var dataClick = $(e.currentTarget).data('click');
-		
-		if(dataClick === 'action_flush'){
-			TweenMax.allTo(circles, 0.3, {top:'50%', left:'45%', ease:'Expo.easeInOut', onComplete:function(){
-				TweenMax.to(this.target, 0.15, {opacity:0})
-			}}, 0.02, function(){
-				$(circles).remove();
-				circles = [];
-			});
-		} else if(dataClick === 'action_follow'){
-			stage.off('mouseup', mousePress);
+    var stage           = $('#stage'),
+        tools           = $('#tools'),
+        appName         = $("#appName"),
+        circles         = [],
+        circlesPerBurst = 10,
+        maxCircles      = circlesPerBurst * 10,
+        holdCount       = 0,
+        holdMax         = 5,
+        holdInterval    = 500,
+        holdTimer;
 
-			$(stage).on('click', function(){
-				$(window).off('mousemove');
+	function init () {
+        $(window).on('keyup', keyPress);
+    	stage.on('mouseup', mousePress);
+        //stage.on('mousedown', mouseDown);
+        tools.on('click', 'button', onToolsClick);
+        setInterval(changeAppNameColor, 3000);
+    }
 
-				TweenMax.allTo(circles, 0.3, {left:'-300px'}, 0.05, function(){
-					$(circles).remove();
-					circles = [];
-					stage.off('click');
-					stage.on('mouseup', mousePress);
-				});
-			});
-
-			$(window).on('mousemove', function(e){
-				TweenMax.allTo(circles, 0.3, {top:e.clientY, left: e.clientX}, 0.02);
-			});
-		} else {
-			
-		}
-	});
-
-	setInterval(changeAppNameColor, 3000);
-	
 	// Respondes to stage when a keyboard key is pressed
 	function keyPress(e){
-		if(KeyDecoder.decode(e.keyCode)){
+		if(KeyDecoder.decode(e.keyCode) && circles.length < maxCircles){
 			createCircle(e);
 		}
 	}
-	
+
+    function mouseDown (e) {
+        stage.on('mouseup', onHoldStop);
+        holdTimer = setInterval(function () {
+            if (holdCount < holdMax) {
+                holdCount++;
+                TweenMax.to(stage, holdInterval / 100, {
+                    top: 0 - (100 * holdCount),
+                    backgroundColor: Utils.generateRandomColor()
+                });
+            } else {
+                onMaxHold();
+            }
+        }, holdInterval);
+    }
+
+    function onMaxHold () {
+        TweenMax.to(stage, 0.1, { backgroundColor: '#FFFFFF', yoyo: true, repeat: -1 });
+    }
+
+    function onHoldStop (e) {
+        holdCount = 0;
+        TweenMax.to(stage, 0.5, { backgroundColor: '#212121' });
+        clearInterval(holdTimer);
+    }
+
 	// Responds to clicks inside the stage
-	function mousePress(e){
-		if(e.clientX && e.clientY){
+	function mousePress (e) {
+		if(e.clientX && e.clientY && circles.length < maxCircles){
+            console.log('Mouse Pressed - Generating Circle Burst');
 			createCircleBurst(e);
 		}
 	}
-	
+
 	// Creates a single circle
 	function createCircle(e){
 		var circle = new Circle(e);
 		stage.append(circle);
 		circles.push(circle);
 	}
-	
-	// Creates a circle burst 
+
+	// Creates a circle burst
 	function createCircleBurst(e){
         for(var i = 0; i < circlesPerBurst; i++){
-            var circle = new Circle(e, { 
-            	fade:false
+            var circle = new Circle(e, {
+            	fade: false
             });
             stage.append(circle);
             circles.push(circle);
         }
     }
-	
+
 	// Changes the app name color in a predefined interval
-	function changeAppNameColor(e){
+	function changeAppNameColor(e) {
 		TweenMax.to(appName, 0.5, {color:Utils.generateRandomColor()})
 	}
+
+    function onToolsClick (e) {
+		var dataClick = $(e.currentTarget).data('click');
+		return (dataClick === 'action_flush') ? flushCircles() : circleFollow();
+    }
+
+    function circleFollow () {
+        stage.off('mouseup', mousePress);
+
+        $(stage).on('click', function () {
+            stage.off('click');
+            $(window).off('mousemove');
+            stage.on('mouseup', mousePress);
+            explodeCircles();
+        });
+
+        $(window).on('mousemove', function (e) {
+            TweenMax.allTo(circles, 0.3, {top:e.clientY, left: e.clientX}, 0.02);
+        });
+    }
+
+    function explodeCircles () {
+        Array.prototype.forEach.call(circles, function (circle) {
+            TweenMax.to( circle, 0.3, {
+                left:    Utils.generateRandomX(),
+                top:     Utils.generateRandomY(),
+                opacity: 0,
+                ease:    'Expo.easeInOut',
+                onComplete: function () {
+                    cleanCircles(this.target);
+                }
+            });
+        });
+    }
+
+    function flushCircles () {
+        TweenMax.allTo(circles, 0.3, {top:'45%', left:'45%', ease:'Expo.easeInOut', onComplete:function () {
+            TweenMax.to(this.target, 0.15, {
+                opacity:0
+            })
+        }}, 0.02, cleanCircles);
+    }
+
+    function cleanCircles (circle) {
+        (circle) ? console.log('Cleaning Circle') : console.log('Cleaning Circles');
+        if (circle) {
+            $(circle).remove();
+            circles.splice(circles.indexOf(circle), 1);
+        } else {
+            $(circles).remove();
+            circles = [];
+        }
+    }
+
+    init();
 });
 
 var Utils = function(){
 
-	var generateRandomColor = function(){
-		return '#'+Math.floor(Math.random()*16777215).toString(16);
+	var generateRandomColor = function () {
+		return '#'+Math.floor(Math.random() * 16777215).toString(16);
 	};
 
-	var generateRandomX = function(){
+	var generateRandomX = function () {
 		return Math.floor(Math.random() * ($(window).width() - 1) + 1);
 	};
 
-	var generateRandomY = function(){
+	var generateRandomY = function () {
 		return Math.floor(Math.random() * ($(window).height() - 1) + 1);
 	};
 
 	return {
 		generateRandomColor: generateRandomColor,
-		generateRandomX: generateRandomX,
-		generateRandomY: generateRandomY
+		generateRandomX:     generateRandomX,
+		generateRandomY:     generateRandomY
 	}
 }();
